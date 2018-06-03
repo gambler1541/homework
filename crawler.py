327bb8e246fb8f3a887682abde19a47ce2f52bcf
pmb <gambler1541@gmail.com>
2018-06-03 16:40:28 +0900

    검색기능 다시만들었다..

--- crawler.py
+++ crawler.py
@@ -1,40 +1,59 @@
-import os
-import requests
-from bs4 import BeautifulSoup
+from utils import *
 
 
-def webtoon_name_crawler(webname):
+
+def webtoon_name_crawler():
+    # 결과값으로 웹툰이름을 키로, id를 값으로 갖는 딕셔너리를 리턴
+    # file_path에 해당하는 파일이 있다면 open
+    # 없다면 생성후 open
     file_path = 'data/webotoon_list.html'
     webtoon_url = 'https://comic.naver.com/webtoon/weekday.nhn'
     if os.path.exists(file_path):
         html = open(file_path, 'rt').read()
     else:
         response = requests.get(webtoon_url)
-        html = response.text
-        open(file_path,'wt').write(html)
+        html_res = response.text
+        html = open(file_path, 'wt').write(html_res)
 
     soup = BeautifulSoup(html, 'lxml')
-    webtoon_title = soup.select('a.title')
-    webtoon_name_list = []
-    for webtoon_name in webtoon_title:
-        webtoon_name_list.append(webtoon_name.get_text(strip=True))
-        webtoon_list = set(webtoon_name_list)
+    webtoon_tag = soup.select('a.title')
+    p = re.compile(r'/.*titleId=(\d+).*')
+    webname_id_dict = dict()
+    for title, id in zip(webtoon_tag, webtoon_tag):
+        id_search = id.get('href')
+        title_id = re.findall(p, id_search)
+        webtoon_name = title.get_text()
+        webname_id_dict[webtoon_name] = title_id
+    return webname_id_dict
 
-    return list(webtoon_list)
 
 
 if __name__=='__main__':
     while True:
-        webtoon_title = input('0: 검색종료 \n'
+        webtoon_title = input('CTRL + C로 종료합니다 \n'
                               '검색할 웹툰명을 입력해 주세요: ')
-        if webtoon_title == '0':
-            break
-        else:
-            webtoon_search = webtoon_name_crawler(webtoon_title)
-            i = 0
-            for webtoon_search_title in webtoon_search:
-                if webtoon_title in webtoon_search_title:
-                    i += 1
-                    print(f'{i}: {webtoon_search_title}')
-        select = input('0: 이전으로 돌아가기\n'
-                       '선택: ')
+        creat_list = webtoon_name_crawler()
+        for webname in creat_list:
+            if webtoon_title in webname:
+                print(f'{webname}')
+
+        select = input('상세보기 할 웹툰: ')
+        for title,test_id in creat_list.items():
+            if select == title:
+                print(f'현재 선택된 웹툰: {title}')
+                webtoon = Webtoon(''.join(test_id))
+                webtoon.set_info()
+                print(f'{webtoon.title}\n'
+                      f'  작가: {webtoon.author}\n'
+                      f'  설명: {webtoon.description}')
+
+
+
+
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,16 +1,85 @@
+import re
 from bs4 import BeautifulSoup
 import requests
 import os
 
 
 class Webtoon:
-    pass
+    def __init__(self,title_id):
+        self.title_id = title_id
+        self._title = None
+        self._author = None
+        self._description = None
+        self._episode_list = list()
+        self._html = ''
+
+    @property
+    def html(self):
+        if not self._html:
+            file_path = 'data/episode_list-{}.html'.format(self.title_id)
+            url_episode_list = 'https://comic.naver.com/webtoon/list.nhn'
+            params = {
+                'titleId': self.title_id
+            }
+
+            if os.path.exists(file_path):
+                html = open(file_path, 'rt').read()
+
+            else:
+
+                response = requests.get(url_episode_list, params)
+                html = response.text
+                open(file_path, 'wt').write(html)
+
+            self._html = html
+        return self._html
+
+    def _get_info(self, attr_name):
+        if not getattr(self, attr_name):
+            self.set_info()
+        return getattr(self, attr_name)
+
+
+    @property
+    def title(self):
+        return self._get_info('_title')
+
+    @property
+    def author(self):
+        return self._get_info('_author')
+
+    @property
+    def description(self):
+        return self._get_info('_description')
+
+
+
+    def set_info(self):
+        soup = BeautifulSoup(self.html, 'lxml')
+        h2_title = soup.select_one('div.detail > h2')
+        title = h2_title.contents[0].strip()
+        author = h2_title.contents[1].get_text(strip=True)
+        description = soup.select_one('div.detail > p').get_text(strip=True)
+
+        self._title = title
+        self._author = author
+        self._description = description
+
+
+
+
+
+
 
 
 class Episode:
     pass
 
 
+
 class EpisodeImage:
     pass
+
+
+
 
